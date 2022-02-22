dff6d684dffa769fecefe5724539232d4674b668
MLiew <30465494+MLiew@users.noreply.github.com>
2022-02-22 13:49:16 +0800

    Update GaussianLowPassFilter.m

--- MATLAB Image Processing GUI/GaussianLowPassFilter.m
+++ MATLAB Image Processing GUI/GaussianLowPassFilter.m
@@ -1,6 +1,4 @@
 % --- Gaussian Low Pass Filter --- %
-% Name: Liew Ying Jia
-% Student ID: LS2015201
 
 function final_img = GaussianLowPassFilter(input_img, d0)
 [row, col, channels] = size(input_img);
